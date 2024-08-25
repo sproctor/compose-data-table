3fa55402387da64deef16dfe39d360fe7806b450
Sean Proctor <sproctor@gmail.com>
2024-08-25 13:14:26 -0400

    refactor demo, add an empty header

--- demo-common/src/commonMain/kotlin/com/seanproctor/datatable/demo/App.kt
+++ demo-common/src/commonMain/kotlin/com/seanproctor/datatable/demo/App.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.seanproctor.datatable.DataColumn
+import com.seanproctor.datatable.DataTableScope
+import com.seanproctor.datatable.TableColumnWidth
 import com.seanproctor.datatable.material3.DataTable
 import com.seanproctor.datatable.material3.PaginatedDataTable
 import com.seanproctor.datatable.paging.rememberPaginatedDataTableState
@@ -44,109 +46,78 @@ fun App(onRowClick: (Int) -> Unit) {
             else -> throw IllegalStateException("Invalid column index")
         }
 
+        val columns = listOf(
+            DataColumn(width = TableColumnWidth.Wrap) { },
+            DataColumn(
+                width = TableColumnWidth.Wrap,
+                onSort = { index, ascending ->
+                    sortColumnIndex = index
+                    sortAscending = ascending
+                }
+            ) {
+                Text("Column1")
+            },
+            DataColumn(
+                alignment = Alignment.CenterEnd,
+                isSortIconTrailing = false,
+                onSort = { index, ascending ->
+                    sortColumnIndex = index
+                    sortAscending = ascending
+                }
+            ) {
+                Text("Column2")
+            }
+        )
+
         if (selectedIndex == 0) {
             DataTable(
-                columns = listOf(
-                    DataColumn(
-                        onSort = { index, ascending ->
-                            sortColumnIndex = index
-                            sortAscending = ascending
-                        }
-                    ) {
-                        Text("Column1")
-                    },
-                    DataColumn(
-                        alignment = Alignment.CenterEnd,
-                        isSortIconTrailing = false,
-                        onSort = { index, ascending ->
-                            sortColumnIndex = index
-                            sortAscending = ascending
-                        }
-                    ) {
-                        Text("Column2")
-                    },
-                ),
+                columns = columns,
                 sortColumnIndex = sortColumnIndex,
                 sortAscending = sortAscending,
                 modifier = Modifier.fillMaxWidth().padding(16.dp),
                 logger = { println(it) }
             ) {
-                sortedData.forEachIndexed { index, data ->
-                    row {
-                        onClick = { onRowClick(index) }
-                        cell {
-                            Text(data.text, maxLines = 1, overflow = TextOverflow.Ellipsis)
-                        }
-                        cell {
-                            Text(data.value.toString())
-                        }
-                    }
-                }
-                row {
-                    isFooter = true
-                    cell { }
-                    cell {
-                        Text(
-                            text = "Footer",
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                    }
-                }
+                generateTable(sortedData, onRowClick)
             }
         } else {
             PaginatedDataTable(
-                columns = listOf(
-                    DataColumn(
-                        onSort = { index, ascending ->
-                            sortColumnIndex = index
-                            sortAscending = ascending
-                        }
-                    ) {
-                        Text("Column1")
-                    },
-                    DataColumn(
-                        alignment = Alignment.CenterEnd,
-                        isSortIconTrailing = false,
-                        onSort = { index, ascending ->
-                            sortColumnIndex = index
-                            sortAscending = ascending
-                        }
-                    ) {
-                        Text("Column2")
-                    },
-                ),
+                columns = columns,
                 state = rememberPaginatedDataTableState(5),
                 sortColumnIndex = sortColumnIndex,
                 sortAscending = sortAscending,
                 modifier = Modifier.fillMaxWidth().padding(16.dp),
                 logger = { println(it) }
             ) {
-                sortedData.forEachIndexed { index, data ->
-                    row {
-                        onClick = { onRowClick(index) }
-                        cell {
-                            Text(data.text, maxLines = 1, overflow = TextOverflow.Ellipsis)
-                        }
-                        cell {
-                            Text(data.value.toString())
-                        }
-                    }
-                }
-//                row {
-//                    isFooter = true
-//                    cell { }
-//                    cell {
-//                        Text(
-//                            text = "Footer",
-//                            maxLines = 1,
-//                            overflow = TextOverflow.Ellipsis,
-//                            style = MaterialTheme.typography.titleLarge
-//                        )
-//                    }
-//                }
+                generateTable(sortedData, onRowClick)
             }
+        }
+    }
+}
+
+fun DataTableScope.generateTable(data: List<DemoData>, onRowClick: (Int) -> Unit) {
+    data.forEachIndexed { index, rowData ->
+        row {
+            onClick = { onRowClick(index) }
+            cell { }
+            cell {
+                Text(rowData.text, maxLines = 1, overflow = TextOverflow.Ellipsis)
+            }
+            cell {
+                Text(rowData.value.toString())
+            }
+        }
+    }
+    row {
+        isFooter = true
+        cell { }
+        cell { }
+        cell {
+            Text(
+                text = "Footer",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.titleLarge
+            )
         }
     }
 }
