2476bd09e17c8b78d7327fc175daf5eb4ac20049
Sean Proctor <sproctor@gmail.com>
2025-02-17 10:15:04 -0500

    alternate background colors

--- demo-common/src/commonMain/kotlin/com/seanproctor/datatable/demo/App.kt
+++ demo-common/src/commonMain/kotlin/com/seanproctor/datatable/demo/App.kt
@@ -20,113 +20,130 @@ import kotlin.math.min
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App(onRowClick: (Int) -> Unit) {
-    Column {
-        var selectedIndex by remember { mutableStateOf(0) }
-        PrimaryTabRow(
-            selectedTabIndex = selectedIndex
-        ) {
-            val titles = listOf("Normal", "Paginated", "Lazy paginated")
-            titles.forEachIndexed { index, title ->
-                Tab(
-                    selected = index == selectedIndex,
-                    onClick = { selectedIndex = index },
-                    text = { Text(text = title, maxLines = 1, overflow = TextOverflow.Ellipsis) }
-                )
-            }
-        }
-        val rowData = (0 until 30).map { index ->
-            DemoData(index + 1f, "index: $index")
-        }
-        var sortColumnIndex by remember { mutableStateOf<Int?>(null) }
-        var sortAscending by remember { mutableStateOf(true) }
-
-        val sortedData = when (sortColumnIndex) {
-            null -> rowData
-            1 -> if (sortAscending) rowData.sortedBy { it.text } else rowData.sortedByDescending { it.text }
-            2 -> if (sortAscending) rowData.sortedBy { it.value } else rowData.sortedByDescending { it.value }
-            else -> throw IllegalStateException("Invalid column index")
-        }
-
-        val columns = listOf(
-            DataColumn(width = TableColumnWidth.Wrap) { },
-            DataColumn(
-                width = TableColumnWidth.Wrap,
-                onSort = { index, ascending ->
-                    sortColumnIndex = index
-                    sortAscending = ascending
-                }
+    Surface {
+        Column {
+            var selectedIndex by remember { mutableStateOf(0) }
+            PrimaryTabRow(
+                selectedTabIndex = selectedIndex
             ) {
-                Text("Column1")
-            },
-            DataColumn(
-                alignment = Alignment.CenterEnd,
-                isSortIconTrailing = false,
-                onSort = { index, ascending ->
-                    sortColumnIndex = index
-                    sortAscending = ascending
+                val titles = listOf("Normal", "Paginated", "Lazy paginated")
+                titles.forEachIndexed { index, title ->
+                    Tab(
+                        selected = index == selectedIndex,
+                        onClick = { selectedIndex = index },
+                        text = { Text(text = title, maxLines = 1, overflow = TextOverflow.Ellipsis) }
+                    )
                 }
-            ) {
-                Text("Column2")
             }
-        )
+            val rowData = (0 until 30).map { index ->
+                DemoData(index + 1f, "index: $index")
+            }
+            var sortColumnIndex by remember { mutableStateOf<Int?>(null) }
+            var sortAscending by remember { mutableStateOf(true) }
 
-        val scrollState = remember(selectedIndex) { DataTableState() }
-        if (selectedIndex == 0) {
-            Box {
-                DataTable(
+            val sortedData = when (sortColumnIndex) {
+                null -> rowData
+                1 -> if (sortAscending) rowData.sortedBy { it.text } else rowData.sortedByDescending { it.text }
+                2 -> if (sortAscending) rowData.sortedBy { it.value } else rowData.sortedByDescending { it.value }
+                else -> throw IllegalStateException("Invalid column index")
+            }
+
+            val columns = listOf(
+                DataColumn(width = TableColumnWidth.Wrap) { },
+                DataColumn(
+                    width = TableColumnWidth.Wrap,
+                    onSort = { index, ascending ->
+                        sortColumnIndex = index
+                        sortAscending = ascending
+                    }
+                ) {
+                    Text("Column1")
+                },
+                DataColumn(
+                    alignment = Alignment.CenterEnd,
+                    isSortIconTrailing = false,
+                    onSort = { index, ascending ->
+                        sortColumnIndex = index
+                        sortAscending = ascending
+                    }
+                ) {
+                    Text("Column2")
+                }
+            )
+
+            val scrollState = remember(selectedIndex) { DataTableState() }
+            if (selectedIndex == 0) {
+                Box {
+                    DataTable(
+                        columns = columns,
+                        state = scrollState,
+                        sortColumnIndex = sortColumnIndex,
+                        sortAscending = sortAscending,
+                        modifier = Modifier.fillMaxWidth().padding(16.dp),
+                        rowBackgroundColor = {
+                            if (it == sortedData.size) {
+                                MaterialTheme.colorScheme.surface
+                            } else if (it % 2 == 0) {
+                                MaterialTheme.colorScheme.surfaceDim
+                            } else {
+                                MaterialTheme.colorScheme.surfaceBright
+                            }
+                        }
+//                    logger = { println(it) }
+                    ) {
+                        generateTable(sortedData, onRowClick)
+                    }
+                    LaunchedEffect(scrollState.horizontalScrollState.viewportSize) {
+                        println("viewport: ${scrollState.horizontalScrollState.viewportSize}")
+                        println("total size: ${scrollState.horizontalScrollState.totalSize}")
+                    }
+                    VerticalScrollbar(
+                        scrollState.verticalScrollState,
+                        Modifier.fillMaxHeight().align(Alignment.CenterEnd)
+                    )
+                    HorizontalScrollbar(
+                        scrollState.horizontalScrollState,
+                        Modifier.fillMaxWidth().align(Alignment.BottomCenter)
+                    )
+                }
+            } else if (selectedIndex == 1) {
+                PaginatedDataTable(
                     columns = columns,
-                    state = scrollState,
+                    state = rememberPaginatedDataTableState(5),
                     sortColumnIndex = sortColumnIndex,
                     sortAscending = sortAscending,
                     modifier = Modifier.fillMaxWidth().padding(16.dp),
-//                    logger = { println(it) }
+                    logger = { println(it) }
                 ) {
                     generateTable(sortedData, onRowClick)
                 }
-                LaunchedEffect(scrollState.horizontalScrollState.viewportSize) {
-                    println("viewport: ${scrollState.horizontalScrollState.viewportSize}")
-                    println("total size: ${scrollState.horizontalScrollState.totalSize}")
-                }
-                VerticalScrollbar(scrollState.verticalScrollState, Modifier.fillMaxHeight().align(Alignment.CenterEnd))
-                HorizontalScrollbar(scrollState.horizontalScrollState, Modifier.fillMaxWidth().align(Alignment.BottomCenter))
-            }
-        } else if (selectedIndex == 1) {
-            PaginatedDataTable(
-                columns = columns,
-                state = rememberPaginatedDataTableState(5),
-                sortColumnIndex = sortColumnIndex,
-                sortAscending = sortAscending,
-                modifier = Modifier.fillMaxWidth().padding(16.dp),
-                logger = { println(it) }
-            ) {
-                generateTable(sortedData, onRowClick)
-            }
-        } else {
-            LazyPaginatedDataTable(
-                columns = columns,
-                state = rememberPaginatedDataTableState(initialPageSize = 5, initialCount = sortedData.size),
-                sortColumnIndex = sortColumnIndex,
-                sortAscending = sortAscending,
-                modifier = Modifier.fillMaxWidth().padding(16.dp),
-                logger = { println(it) },
-                fetchPage = { state ->
-                    val fromIndex = state.pageIndex * state.pageSize
-                    val toIndex = min(fromIndex + state.pageSize, sortedData.size)
-                    val subset = sortedData.subList(fromIndex, toIndex)
-                    subset.forEachIndexed { index, rowData ->
-                        row {
-                            onClick = { onRowClick(fromIndex + index) }
-                            cell { }
-                            cell {
-                                Text(rowData.text, maxLines = 1, overflow = TextOverflow.Ellipsis)
-                            }
-                            cell {
-                                Text(rowData.value.toString())
+            } else {
+                LazyPaginatedDataTable(
+                    columns = columns,
+                    state = rememberPaginatedDataTableState(initialPageSize = 5, initialCount = sortedData.size),
+                    sortColumnIndex = sortColumnIndex,
+                    sortAscending = sortAscending,
+                    modifier = Modifier.fillMaxWidth().padding(16.dp),
+                    logger = { println(it) },
+                    fetchPage = { state ->
+                        val fromIndex = state.pageIndex * state.pageSize
+                        val toIndex = min(fromIndex + state.pageSize, sortedData.size)
+                        val subset = sortedData.subList(fromIndex, toIndex)
+                        subset.forEachIndexed { index, rowData ->
+                            row {
+                                onClick = { onRowClick(fromIndex + index) }
+                                cell { }
+                                cell {
+                                    Text(rowData.text, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                                }
+                                cell {
+                                    Text(rowData.value.toString())
+                                }
                             }
                         }
                     }
-                }
-            )
+                )
+            }
         }
     }
 }
